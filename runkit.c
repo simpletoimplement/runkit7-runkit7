1e3bbd6a99f4cf718f0c95b53fbf216fe1926797
Tyson Andre <tysonandre775@hotmail.com>
2017-06-07 22:56:58 -0700

    Fix ReflectionFunction info for runkit_constant_add

--- runkit.c
+++ runkit.c
@@ -171,7 +171,7 @@ ZEND_BEGIN_ARG_INFO_EX(arginfo_runkit_constant_remove, 0, 0, 1)
 	ZEND_ARG_INFO(0, constname)
 ZEND_END_ARG_INFO()
 
-ZEND_BEGIN_ARG_INFO_EX(arginfo_runkit_constant_add, 0, 0, 1)
+ZEND_BEGIN_ARG_INFO_EX(arginfo_runkit_constant_add, 0, 0, 2)
 	ZEND_ARG_INFO(0, constname)
 	ZEND_ARG_INFO(0, value)
 	ZEND_ARG_INFO(0, newVisibility)
