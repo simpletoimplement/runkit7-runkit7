fb26cb56a222e78dac518a292683d5c57f05d58a
Tyson Andre <tysonandre775@hotmail.com>
2017-11-04 09:43:39 -0700

    Bump php 7.2.0 to RC5 for travis tests

--- ci/generate_php_install_dir.sh
+++ ci/generate_php_install_dir.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -xeu
 # Print a folder name based on the integer width(32 bit or 64 bit), whether or not NTS is used (implied by PHP_CONFIGURE_ARGS), and PHP_CONFIGURE_ARGS.
 PHP_NTS_NORMAL_VERSION=${PHP_NTS_VERSION//RC[0-9]/}
-if [ "$PHP_NTS_NORMAL_VERSION" = "7.1.0" ]; then
-	PHP_NTS_NORMAL_VERSION=7.1.0RC1
+if [ "$PHP_NTS_NORMAL_VERSION" = "7.2.0" ]; then
+	PHP_NTS_NORMAL_VERSION=7.2.0RC5
 fi
 PHP_INSTALL_DIR="$HOME/travis_cache/php-$PHP_NTS_NORMAL_VERSION"
 if [ "${USE_32BIT:-0}" = "1" ]; then
