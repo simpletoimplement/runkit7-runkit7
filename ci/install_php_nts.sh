99aedb9a806251fc7800bcf7d58137e0d576b50b
Tyson Andre <tysonandre775@hotmail.com>
2018-10-17 00:13:15 -0400

    Fix error in PHP installation script

--- ci/install_php_nts.sh
+++ ci/install_php_nts.sh
@@ -1,16 +1,16 @@
 #!/bin/bash -xeu
 
 echo "Attempting to install NTS PHP, NTS version '$PHP_NTS_VERSION'/ configure args '$PHP_CONFIGURE_ARGS'"
-if [ "x$PHP_NTS_VERSION" = "x"; then
+if [[ "x$PHP_NTS_VERSION" = "x" ]]; then
 	echo "Missing PHP_NTS_VERSION"
 	exit 1
-elif [ "x$PHP_CONFIGURE_ARGS" = "x" ] ; then
+elif [[ "x$PHP_CONFIGURE_ARGS" = "x" ]] ; then
 	echo "Missing PHP_CONFIGURE_ARGS"
 	exit 1
 fi
 PHP_INSTALL_DIR="$(./ci/generate_php_install_dir.sh)"
 echo "Downloading $PHP_INSTALL_DIR\n"
-if [ -x $PHP_INSTALL_DIR/bin/php ] ; then
+if [[ -x $PHP_INSTALL_DIR/bin/php ]] ; then
 	echo "PHP $PHP_NTS_VERSION already installed and in cache at $PHP_INSTALL_DIR";
 	exit 0
 fi
@@ -20,13 +20,13 @@ PHP_NTS_NORMAL_VERSION=${PHP_NTS_NORMAL_VERSION//beta[0-9]/}
 PHP_FOLDER="php-$PHP_NTS_VERSION"
 
 # Remove cache if it somehow exists
-if [ "x${TRAVIS:-0}" != "x" ]; then
+if [[ "x${TRAVIS:-0}" != "x" ]]; then
 	rm -rf $HOME/travis_cache/
 fi
 # Otherwise, put a minimal installation inside of the cache.
 PHP_TAR_FILE="$PHP_FOLDER.tar.bz2"
-if [ "$PHP_NTS_NORMAL_VERSION" == "7.3.0" || "$PHP_NTS_NORMAL_VERSION" == "7.4.0" ] ; then
-    if [ "$PHP_NTS_NORMAL_VERSION" == "7.3.0" ] ; then
+if [[ "$PHP_NTS_NORMAL_VERSION" == "7.3.0" || "$PHP_NTS_NORMAL_VERSION" == "7.4.0" ]] ; then
+    if [[ "$PHP_NTS_NORMAL_VERSION" == "7.3.0" ]] ; then
         GIT_BRANCH=7.3
     else
         GIT_BRANCH=master
