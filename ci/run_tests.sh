eaf984bf839c19a3bcebc6e4b5cadff462438a00
Tyson Andre <tysonandre775@hotmail.com>
2019-03-23 19:00:44 -0400

    Run tests in parallel in Travis CI
    The Travis tests include valgrind,
    which makes those tests noticeably slow.
    
    Use as many processes as are available (2 on Travis)

--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -1,6 +1,12 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 export REPORT_EXIT_STATUS=1
-export TESTS=--show-diff
+export TESTS='--show-diff'
+PROCESSES="$(nproc)"
+if [[ "$PROCESSES" -gt 1 ]]; then
+    TESTS="$TESTS -j$PROCESSES"
+fi
+
+cp ci/run-tests-parallel.php run-tests.php
 
 make test
